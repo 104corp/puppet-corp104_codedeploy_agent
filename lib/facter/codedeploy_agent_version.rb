df793ffa7cd9734e2bfc656ec96cb76b7c19e533
scott.liao <shazi7804@gmail.com>
2018-10-07 01:25:38 +0800

    update agent version for facter

--- lib/facter/codedeploy_agent_version.rb
+++ lib/facter/codedeploy_agent_version.rb
@@ -4,7 +4,7 @@ Facter.add(:codedeploy_agent_version) do
   confine { Facter.value(:kernel) != 'windows' }
   confine { Facter.value(:operatingsystem) != 'nexus' }
   setcode do
-    if Facter::Util::Resolution.exec('test -e /opt/codedeploy-agent/.version')
+    if File.exists?('/opt/codedeploy-agent/.version')
       Facter::Core::Execution.exec('cat /opt/codedeploy-agent/.version 2>&1').match(/^agent_version:\ OFFICIAL_(\d+\.\d+\-\d+\.\d+).*$/)[1]
     end
   end
